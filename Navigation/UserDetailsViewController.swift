78bc99ae8245089c9fef3eb748a0e1ee45141bf1
moooove08 <tj.vlad@gmail.com>
2023-10-20 14:00:20 +0300

    Initial Commit

---
+++ Navigation/UserDetailsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  UserDetailsViewController.swift
+//  Navigation
+//
+//  Created by Vlad Kuzmenko on 20.10.2023.
+//
+
+import UIKit
+
+class UserDetailsViewController: UIViewController {
+
+    @IBOutlet var userInfo: UIView!
+    
+    @IBOutlet var userInfoLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Navigation/UserListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Vlad Kuzmenko on 18.10.2023.
+//
+
+import UIKit
+
+class UserListViewController: UIViewController {
+
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var users = ["Vlad","Myrasha","Vitalyi", "Alyona","Matvey","Anya"]
+
+    
+    
+  
+    
+    
+
+}
+
